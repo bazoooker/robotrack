fba84a179e086072330b1710a28a3423beda78e2
Bazoooker <manakov55@yandex.ru>
2020-02-19 16:32:11 +0700

    all wave decos

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -4,6 +4,25 @@
 $(document).ready(function() {
 
 
+    // 1. накидывать классы для изменения цветов
+    // 2. менять js-ом координаты градиентов
+
+
+
+
+
+    $('.js-change-page-color').click(function() {
+        
+        // $('#first').addClass('green');
+        // setTimeout(function() {
+        //     $('#paint0_linear').attr('x1', '577.5');
+        //     $('#paint0_linear').attr('y1', '-124');
+        //     $('#paint0_linear').attr('x2', '-176.967');
+        //     $('#paint0_linear').attr('y2', '-110.441');
+        // }, 1000);
+    });
+
+
 
 	
 	// появление деко	
